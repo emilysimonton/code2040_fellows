cea1e17131d34e43ae68b578ca64765c8602f3b0
shushugah <mathsolver24@gmail.com>
2014-08-05 16:58:39 -0400

    moved rake tasks to seed file, and imported image file_names

--- Rakefile
+++ Rakefile
@@ -19,11 +19,19 @@ Rails.application.load_tasks
         :title => row[1],
         :company_name => row[2],
         :bio => row[3],
-        :twitter_handle => row[4]        
+        :twitter_handle => row[4]       
       })
-      
     end
     Fellow.first.destroy
     puts "Added row, expect 24 fellows"
   end
 
+task :import_photos => :environment do
+source_path = Rails.root.join('db', 'fellow_2013_images')
+
+Dir.glob("#{source_path}/*.png").each do |image_file_name|
+	base_name=File.basename(image_file_name, ".png")
+	Fellow.where('name ILIKE ?', "#{base_name}%").update_all(profile_pic: base_name)
+	Fellow.find_by(name: "Estefania Ortiz").update_attributes(profile_pic: "Estefania.jpg")
+end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+    csv_file_path = 'db/code2040_2014_bios_sheet1.csv'
+
+    CSV.foreach(csv_file_path) do |row|
+      Fellow.create!({
+        :name => row[0],
+        :title => row[1],
+        :company_name => row[2],
+        :bio => row[3],
+        :twitter_handle => row[4]       
+      })
+    end
+    Fellow.first.destroy
+    puts "Added row, expect 24 fellows"
+
+source_path = Rails.root.join('db', 'fellow_2013_images')
+
+Dir.glob("#{source_path}/*").each do |image_file_name|
+	base_name=File.basename(image_file_name, File.extname(image_file_name))
+	#this allows us to use an image with any extension
+	Fellow.where('name ILIKE ?', "#{base_name}%").update_all(profile_pic: File.basename(image_file_name))
+	# binding.pry
+	# Fellow.find_by(name: "Estefania Ortiz").update_attributes(profile_pic: "Estefania.jpg")
+end
