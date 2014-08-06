bd3115e3632a124b0c4febbce8869c5f10401c93
emilysimonton <emily.simonton@gmail.com>
2014-08-06 16:34:00 -0400

    add methods to find current fellows

--- app/models/fellow.rb
+++ app/models/fellow.rb
@@ -4,4 +4,45 @@ class Fellow < ActiveRecord::Base
 		"#{slug}"
 	end
 
+  def self.sort_by_name
+    self.all.sort_by do |fellow|
+      fellow.name
+    end
+  end
+
+  def self.sort_by_year
+    self.all.sort_by do |fellow|
+      fellow.year
+    end
+  end
+
+  def self.current_year
+    fellow = self.sort_by_year.last
+    fellow.year
+  end
+
+  def current_student?
+    true if self.year == Fellow.current_year
+  end
+
+  def self.alumnae
+    alumnae = []
+    self.sort_by_name.all.each do |fellow|
+      if !fellow.current_student?
+        alumnae << fellow
+      end
+    end
+    alumnae
+  end
+
+  def self.current_students
+    current_students = []
+    self.sort_by_name.all.each do |fellow|
+      if fellow.current_student?
+        current_students << fellow
+      end 
+    end
+    current_students
+  end
+    
 end
